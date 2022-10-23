6e9d383396d6486ef3bbdc8a42c836f9b265c976
Joe King <j.king92@gmail.com>
2022-10-23 13:44:54 -0700

    feat: Add the methods to check who is long/short term players, calculate total value, and provide team details

--- lib/team.rb
+++ lib/team.rb
@@ -14,4 +14,37 @@ class Team
   def add_player(player)
     @roster << player
   end
+
+  def long_term_players
+    # should come back and consider other enumerables if time permits
+    long_term_roster = []
+    @roster.each do |player|
+      long_term_roster << player if player.contract_length > 24
+    end
+    long_term_roster
+  end
+
+  def short_term_players
+    # should come back and consider other enumerables if time permits
+    short_term_roster = []
+    @roster.each do |player|
+      short_term_roster << player if player.contract_length <= 24
+    end
+    short_term_roster
+  end
+
+  def total_value
+    total = 0
+    @roster.each do |player|
+      total += player.total_cost
+    end
+    total
+  end
+
+  def details
+    detail = Hash.new
+    detail["total_value"] = total_value
+    detail["player_count"] = player_count
+    detail
+  end
 end
