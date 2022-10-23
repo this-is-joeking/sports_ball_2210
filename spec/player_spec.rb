43a5e2cb597a1b96f3e2c6ad07e336efdbbfbc72
Joe King <j.king92@gmail.com>
2022-10-23 13:06:28 -0700

    test: Add tests for nickname attribute and set_nickname method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,18 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+
+  it 'starts without a nickname' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq(nil)
+  end
+
+  it 'can have a nickname assigned' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+    player.set_nickname!("Squints")
+
+    expect(player.nickname).to eq("Squints")
+  end
 end
